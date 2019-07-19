7ba300f9537386d5d2f5f1129dd0592c846f9668
Myles Wilson <spaceboymyles@yahoo.com>
2019-07-19 15:06:11 -0700

    Started coding a text adventure game. No direct link from the homepage yet.

---
+++ game.js
@@ -0,0 +1,26 @@
+//Fake counter that begins the game.
+var i = 0;
+var done = 0;
+
+function load(){
+	setTimeout(function(){
+        document.getElementById("output").innerHTML = i + "%";
+        i+=10;
+		if(i <= 100){
+			load();
+		}
+		if(i === 110){
+			document.getElementById("output").innerHTML = "LOADED";
+            window.done = 1;
+		}
+	}, 500)
+}
+load();
+
+//User input.
+if (done === 1){
+    var name = prompt("What is your name civilian?", "John Doe");
+    if (name != null) {
+        document.getElementById("output").innerHTML = "Civilian R3-" + name + " please listen carefully:";
+    }
+}
